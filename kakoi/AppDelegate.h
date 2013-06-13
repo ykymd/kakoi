465a08630b0ad6728a4b6be347a3721f95e27f3c
yuki <yuuki940331@yahoo.co.jp>
2013-06-13 15:04:46 +0900

    Initial Commit

---
+++ kakoi/AppDelegate.h
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.h
+//  kakoi
+//
+//  Created by Yuki Yamada on 2013/06/13.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "cocos2d.h"
+
+@interface AppController : NSObject <UIApplicationDelegate, CCDirectorDelegate>
+{
+	UIWindow *window_;
+	UINavigationController *navController_;
+
+	CCDirectorIOS	*director_;							// weak ref
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (readonly) UINavigationController *navController;
+@property (readonly) CCDirectorIOS *director;
+
+@end
